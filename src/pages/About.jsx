b892d0a4007c006bc894edadb3347434df297ca1
Abner-PB <152179406+APB-Abner@users.noreply.github.com>
2024-10-31 18:31:32 -0300

    Refactor About page layout for improved responsiveness and structure

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,12 +3,15 @@ import Eu from "../assets/eu.jpg"
 
 export default function About() {
     return (
-        <div className="container p-4">
-            <div className="d-flex justify-center gap-10">
-                <p className="col-4 text-center content-center">Um apaixonado por tecnologia, sou um ser criativo com adora música, desenho e fotografia. No meu tempo livre, gosto de tocar músicas, treinar, fazer animações e jogar videogame. Minha curiosidade me levou a explorar temas como mitologia, ficção científica e design, tanto nos meus projetos pessoais quanto nos meus hobbies.</p>
-                <img className="col-4 figure-img img-fluid rounded " src={Eu} alt="Abner no sol" />
+        <div className="row d-flex p-4">
+            <div className="col-md-6 col-12 my-3 d-flex justify-center gap-10">
+                <p className="text-center content-center">Um apaixonado por tecnologia, sou um ser criativo com adora música, desenho e fotografia. No meu tempo livre, gosto de tocar músicas, treinar, fazer animações e jogar videogame. Minha curiosidade me levou a explorar temas como mitologia, ficção científica e design, tanto nos meus projetos pessoais quanto nos meus hobbies.</p>
+            </div>
+            <div className="col-md-6 col-12 my-3 justify-center gap-10">
+                <img className="figure-img img-fluid rounded" src={Eu} alt="Abner no sol" />
             </div>
         </div>
     );
 }
 
+//"col-md-6 col-12 d-flex justify-content-center">
