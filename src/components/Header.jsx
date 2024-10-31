5f7f4dd468b3d76e9e7829fa46b8ba6231120b59
Abner-PB <152179406+APB-Abner@users.noreply.github.com>
2024-10-31 19:52:03 -0300

    Refactor Header component for improved mobile responsiveness and code cleanliness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,13 @@
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon, PaintBrushIcon, PuzzlePieceIcon, MusicalNoteIcon, CameraIcon, ArrowPathRoundedSquareIcon, PencilIcon } from '@heroicons/react/24/outline'
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import FlyMenu from './FlyMenu'
 import navigation from '../data/navigation.js'
 import { Link } from 'react-router-dom'
 
 
 
-const solutions = [
-
-];
-
-const callsToAction = [
-
-];
+const solutions = [];
+const callsToAction = [];
 
 
 function classNames(...classes) {
@@ -23,9 +17,9 @@ function classNames(...classes) {
 export default function Header() {
     return (
         <Disclosure as="nav" className="bg-gray-800">
-            <div className="row mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                <div className="d-flex h-16 items-center sm:justify-between">
-                    <div className="inset-y-0 left-2 flex max-h-max items-center sm:hidden">
+            <div className="row mx-auto max-w-7xl px-2 md:px-6 lg:px-8">
+                <div className="d-flex h-16 items-center md:justify-between">
+                    <div className="inset-y-0 left-2 flex max-h-max items-center md:hidden">
                         {/* Mobile menu button */}
                         <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                             <span className="absolute -inset-0.5" />
@@ -34,20 +28,20 @@ export default function Header() {
                             <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block" />
                         </DisclosureButton>
                     </div>
-                    <div className="flex flex-shrink-0 items-center justify-end sm:justify-center">
+                    <div className="flex flex-shrink-0 items-center justify-end md:justify-center">
                         <img
                             alt="Abner"
                             src="logo.svg"
                             className="h-8 w-auto"
                         />
-                        <div className="hidden sm:ml-12 sm:flex sm:items-center sm:justify-end">
+                        <div className="hidden md:ml-12 md:flex md:items-center md:justify-end">
                             <FlyMenu className="flex space-x-6" navigation={navigation} solutions={solutions} callsToAction={callsToAction} />
                         </div>
                     </div>
                 </div>
             </div>
 
-            <DisclosurePanel className="sm:hidden">
+            <DisclosurePanel className="md:hidden">
                 <div className="space-y-1 px-2 pb-3 pt-2">
                     {navigation.map((item) => (
                         <Link
