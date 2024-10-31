5873be85fa97f6b6a5a11e5842046853124ef00d
Abner-PB <152179406+APB-Abner@users.noreply.github.com>
2024-10-31 18:02:43 -0300

    Add memory data module and create Memory page to display categories

---
+++ src/pages/Memory.jsx
@@ -0,0 +1,11 @@
+import Category from "../components/Category";
+import memory from "../data/memory";
+
+export default function Works() {
+    return (
+        <>
+            <Category callouts={memory} />
+        </>
+    );
+}
+
