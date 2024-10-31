526c1035d3763212375e5f2027d64bc6a65b8e5f
Abner-PB <152179406+APB-Abner@users.noreply.github.com>
2024-10-31 18:02:09 -0300

    Add inspirations data module and create Inspirations page to display categories

---
+++ src/data/inspirations.js
@@ -0,0 +1,31 @@
+import A1 from '../assets/1.png'
+import A2 from '../assets/2.png'
+import A3 from '../assets/3.png'
+
+const future = [
+    
+    {
+        name: 'Curto prazo',
+        description: 'Uma longa jornada',
+        imageSrc: A1,
+        imageAlt: 'Imagem futuro curto',
+        href: 'cp',
+    },
+    {
+        name: 'Médio prazo',
+        description: 'Uma longa jornada',
+        imageSrc: A2,
+        imageAlt: 'Imagem futuro médio',
+        href: 'mp',
+    },
+    {
+        name: 'Longo prazo',
+        description: 'Uma longa jornada',
+        imageSrc: A3,
+        imageAlt: 'Imagem futuro longo',
+        href: 'lp',
+    }
+]
+
+
+export default future
---
+++ src/pages/Inspirations.jsx
@@ -0,0 +1,11 @@
+import Category from "../components/Category";
+import inspirations from "../data/inspirations";
+
+export default function Works() {
+    return (
+        <>
+            <Category callouts ={inspirations} />
+        </>
+    );
+}
+
